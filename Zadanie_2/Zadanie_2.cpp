2e8efa55dc6619fea72ad9154074a3b7fccfe965
AlexPlay323 <39780899+AlexPlay323@users.noreply.github.com>
2022-01-15 19:33:13 +0200

    Добавьте файлы проекта.

---
+++ Zadanie_2/Zadanie_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+#include <locale>
+using namespace std;
+//Вводиться число с клавиатуры и считается количество цифр в нем
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int num,i;
+    cout << "Введите число" << '\n';
+    cin >> num;
+    for (i = 0; num != 0; i++) {
+        num /= 10;
+    }
+    cout << i;
+    switch (i) {
+        case 2: cout << " цифры"; break;
+        case 1: cout << " цифра"; break;
+        case 3: cout << " цифры"; break;
+        case 4: cout << " цифры"; break;
+        default: cout << " цифр"; break;
+    }
+    return 0;
+}
